a87a196e174ebde03a1dbda5378e2f9aa46ed5ab
DESKTOP-TD4EK52\Jonas <Jonas.Zimmermann@student.hpi.uni-potsdam.de>
2018-11-04 00:36:23 +0100

    working on issue #9

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -13,12 +13,26 @@ class AuthorsController < ApplicationController
         end
     end
 
+    def edit
+        @author = Author.find(params[:id])
+    end
+
     def index
         @authors = Author.all
     end
 
     def show
         @author = Author.find(params[:id])
+    end
+
+    def update
+        @author =Author.find(params[:id])
+
+        if @author.update(author_params)
+            redirect_to @author
+        else
+            render 'edit'
+        end
     end
 
     private
